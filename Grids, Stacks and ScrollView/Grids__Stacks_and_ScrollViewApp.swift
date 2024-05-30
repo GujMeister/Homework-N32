827540c5b951787c92184115b6c69f2328d439d0
GujMeister <90295019+GujMeister@users.noreply.github.com>
2024-05-30 23:32:09 +0400

    Initial Commit

---
+++ Grids, Stacks and ScrollView/Grids__Stacks_and_ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grids__Stacks_and_ScrollViewApp.swift
+//  Grids, Stacks and ScrollView
+//
+//  Created by Luka Gujejiani on 30.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Grids__Stacks_and_ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
